b9418ad1aab99bd91200a2c30bb42140d300103d
Vivian Wang <wangruikang@iscas.ac.cn>
2026-07-01 11:12:19 +0800

    fix(dbus): restrict After/Requires removal patterns
    While the intention for removing all After= and Requires= lines on
    dbus.service and dbus.socket is to remove the dependency on
    sysinit.target, this unintentionally removes the Requires=dbus.socket
    dependency for dbus.service as well, which is present in both upstream
    dbus and dbus-broker.
    
    Restrict the pattern to only lines mentioning sysinit.target, to better
    match what Ubuntu has added downstream.

--- modules.d/16dbus-daemon/module-setup.sh
+++ modules.d/16dbus-daemon/module-setup.sh
@@ -52,13 +52,15 @@ install() {
 
     # Remove After/Requires=sysinit.target and After=basic.target for initramfs in the dbus service unit.
     sed -i -e \
-        '/^\(After\|DefaultDependencies\|Requires\)=/d
+        '/^\(After\|Requires\)=.*sysinit\.target/d
+        /^DefaultDependencies=/d
         /^\[Unit\]/aDefaultDependencies=no\nConflicts=shutdown.target\nBefore=shutdown.target' \
         "$initdir$systemdsystemunitdir/dbus.service"
 
     # Remove After/Requires=sysinit.target for initramfs in the dbus socket unit.
     sed -i -e \
-        '/^\(After\|DefaultDependencies\|Requires\)=/d
+        '/^\(After\|Requires\)=.*sysinit\.target/d
+        /^DefaultDependencies=/d
         /^\[Unit\]/aDefaultDependencies=no\nConflicts=shutdown.target\nBefore=shutdown.target sockets.target
         /^\[Socket\]/aRemoveOnStop=yes' \
         "$initdir$systemdsystemunitdir/dbus.socket"
