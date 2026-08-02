fe9fd498b6fb85c141b13c8067268e3c04110426
devkontrol <dev@kontrol.dev>
2026-08-02 13:17:23 -0400

    docs: update NEWS.md and AUTHORS for release 112

--- dracut.sh
+++ dracut.sh
@@ -23,7 +23,7 @@
 # Please do not use functions from this file in your dracut module
 # Only use functions from dracut-functions.sh
 
-DRACUT_VERSION="111"
+DRACUT_VERSION="112"
 
 unset BASH_ENV
 unset GZIP
