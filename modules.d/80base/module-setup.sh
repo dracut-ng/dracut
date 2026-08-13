b3f9b188c11571558c7ebc73693dc7a7ecf165d2
Fiona Klute <fiona.klute@gmx.de>
2026-08-13 18:08:46 +0200

    fix(base): install dracut-util from dracutruntimedir
    dracut-util is used inside the initramfs, so it must match the sysroot, if
    any. dracutruntimedir is the location for such executables.

--- modules.d/80base/module-setup.sh
+++ modules.d/80base/module-setup.sh
@@ -55,7 +55,7 @@ install() {
         reboot \
         sysctl
 
-    inst_binary "${dracutbasedir}/dracut-util" "/usr/bin/dracut-util"
+    inst_binary "${dracutruntimedir}/dracut-util" "/usr/bin/dracut-util"
 
     ln -s dracut-util "${initdir}/usr/bin/dracut-getarg"
     ln -s dracut-util "${initdir}/usr/bin/dracut-getargs"
