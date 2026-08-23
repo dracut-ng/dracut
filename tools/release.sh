dracut-ng/dracut

commit 3ca9d54c3319e41a31620c79889447d423e36bf2

chore: remove dracut-ng branding
At this point, the https://github.com/dracut-ng/dracut repository
has established itself as the official upstream repository for the
dracut project.

Consequently, the "dracut-ng" branding is no longer needed to
distinguish it from the original, now-defunct repository.

The term "dracut-ng" should be used only as the GitHub organization
name, not as the repository or project name.

tools/release.sh
@@ -36,7 +36,7 @@ if ! type -p clog &> /dev/null; then
 fi
 head --lines=2 NEWS.md > NEWS_header.md
 tail --lines=+2 NEWS.md > NEWS_body.md
-printf "dracut-ng-%s\n=============\n" "$NEW_VERSION" > NEWS_header_new.md
+printf "dracut-%s\n=============\n" "$NEW_VERSION" > NEWS_header_new.md
 
 # Append the list to the section in `NEWS.md`
 cat CONTRIBUTORS.md NEWS_body.md > NEWS_body_with_conttributors.md

doc_site/antora.yml
@@ -1,6 +1,6 @@
 ---
 name: ROOT
-title: dracut-ng
+title: dracut
 version: ~
 nav:
     - modules/ROOT/nav.adoc

commit ddc3cf7771dd8b04e51f1104272fdb65d417c9f5

fix(NEWS): correct setext heading underline length
Align the setext heading underline length to the title length.

Fixes: 3ca9d54c3319 ("chore: remove dracut-ng branding")

tools/release.sh
@@ -36,7 +36,7 @@ if ! type -p clog &> /dev/null; then
 fi
 head --lines=2 NEWS.md > NEWS_header.md
 tail --lines=+2 NEWS.md > NEWS_body.md
-printf "dracut-%s\n=============\n" "$NEW_VERSION" > NEWS_header_new.md
+printf "dracut-%s\n==========\n" "$NEW_VERSION" > NEWS_header_new.md
 
 # Append the list to the section in `NEWS.md`
 cat CONTRIBUTORS.md NEWS_body.md > NEWS_body_with_conttributors.md