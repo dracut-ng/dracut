dracut-ng/dracut

commit fcf3abd126a242a564742568f4180326ef719bfa

fix(busybox): use full path provided by busybox --list-all
`busybox --list` only lists the names of the included applets but does
not show their paths. The busybox module looks for the path on the
host system. It will skip the applet in case the binary is not found on
the host system.

Rely on `busybox --list-all` to provide the path for the applets. That
way all applets can be installed. Support getting relative paths (like
`bin/busybox`) in addition to absolute paths (like `/bin/busybox`).

modules.d/81busybox/module-setup.sh
@@ -13,26 +13,27 @@ check() {
 
 # called by dracut
 install() {
-    local _i _path _busybox
+    local _path _busybox _busybox_path
     local _dstdir="${dstdir:-"$initdir"}"
     local _progs=()
     _busybox=$(find_binary busybox)
-    inst "$_busybox" /usr/bin/busybox
+    _busybox_path="/usr/bin/busybox"
 
     # do not depend on CONFIG_FEATURE_INSTALLER
     # install busybox symlinks manually
-    for _i in $($_busybox --list); do
-        [[ ${_i} == busybox ]] && continue
-        _progs+=("${_i}")
+    for _path in $($_busybox --list-all); do
+        if [[ ${_path##*/} == busybox ]]; then
+            _busybox_path="/${_path#/}"
+        else
+            _progs+=("/${_path#/}")
+        fi
     done
 
-    for _i in "${_progs[@]}"; do
-        _path=$(find_binary "$_i")
-        [ -z "$_path" ] && continue
-
+    inst "$_busybox" "$_busybox_path"
+    for _path in "${_progs[@]}"; do
         # do not remove existing destination files
-        [ -e "${_dstdir}/$_path" ] && continue
+        [ -e "${_dstdir}$_path" ] && continue
 
-        ln_r /usr/bin/busybox "$_path"
+        ln_r "$_busybox_path" "$_path"
     done
 }

commit 8b9e534f4cd84a68d33755ece3aa26dbb7e9abba

fix(busybox): use --list-full instead of --list-all
That is the documented option name. --list-all also works because the busybox
code only checks if there is a non-\0 character after --list.

[1] https://github.com/vda-linux/busybox_mirror/blob/15666f3e9a0b54305cec1b01f34bc5b2a1794e74/libbb/appletlib.c#L858

Fixes: fcf3abd126a2 ("fix(busybox): use full path provided by busybox --list-all")

modules.d/81busybox/module-setup.sh
@@ -21,7 +21,7 @@ install() {
 
     # do not depend on CONFIG_FEATURE_INSTALLER
     # install busybox symlinks manually
-    for _path in $($_busybox --list-all); do
+    for _path in $($_busybox --list-full); do
         if [[ ${_path##*/} == busybox ]]; then
             _busybox_path="/${_path#/}"
         else