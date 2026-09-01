dracut-ng/dracut

commit c22d7551b095e02daddb0bd91d898050abd09265

fix(Makefile): remove src/dracut-cpio/target/ on clean
cargo creates `src/dracut-cpio/target` when building. Remove the whole
directory on `clean`.

Makefile
@@ -296,7 +296,7 @@ clean:
 	$(RM) dracut-util $(UTIL_OBJECTS)
 	$(RM) $(manpages)
 	$(RM) dracut.pc
-	$(RM) dracut-cpio src/dracut-cpio/target/release/dracut-cpio*
+	$(RM) dracut-cpio src/dracut-cpio/target/
 	$(RM) -rf build/ doc_site/modules/ROOT/pages/man/*
 	$(MAKE) -C test clean
 

commit 291e152891e14c0bc10444bbc2a87670b7e23091

fix(Makefile): use -rf for cleaning src/dracut-cpio/target/
Make adds -f automatically, rm still refuses to delete directories without -r.

Fixes: c22d7551b095e02daddb0bd91d898050abd09265

Makefile
@@ -296,8 +296,8 @@ clean:
 	$(RM) dracut-util $(UTIL_OBJECTS)
 	$(RM) $(manpages)
 	$(RM) dracut.pc
-	$(RM) dracut-cpio src/dracut-cpio/target/
-	$(RM) -rf build/ doc_site/modules/ROOT/pages/man/*
+	$(RM) dracut-cpio
+	$(RM) -rf build/ doc_site/modules/ROOT/pages/man/* src/dracut-cpio/target/
 	$(MAKE) -C test clean
 
 distclean: clean