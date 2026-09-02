dracut-ng/dracut-ng

commit 9fdf683f6d2f7c6ecbd98884f142de86bbea5a56

fix(dracut): ensure hardlink deduplication is reproducible
By default, hardlink will only deduplicate files with identical mtimes,
down to one-second granularity. If a dracut module rapidly generates
multiple identical files, it is completely up to chance as to whether
their mtimes cross a second boundary or not, and thus whether they get
deduplicated or not. This results in non-reproducible output.

Re-order hardlink with respect to clamping to avoid this problem.

Fixes: https://issues.redhat.com/browse/RHEL-101901

dracut.sh
@@ -2259,12 +2259,6 @@ for ((i = 0; i < ${#include_src[@]}; i++)); do
     fi
 done
 
-if [[ $do_hardlink == yes ]] && command -v hardlink > /dev/null; then
-    dinfo "*** Hardlinking files ***"
-    hardlink "$initdir" 2>&1 | ddebug
-    dinfo "*** Hardlinking files done ***"
-fi
-
 # strip binaries
 if [[ $do_strip == yes ]]; then
     # Prefer strip from elfutils for package size
@@ -2461,7 +2455,7 @@ if [[ $uefi == yes ]]; then
 fi
 
 clamp_mtimes() {
-    find "$1" -newer "$dracutbasedir/dracut-functions.sh" -print0 \
+    find "$@" -newer "$dracutbasedir/dracut-functions.sh" -print0 \
         | xargs -r -0 touch -h -m -c -r "$dracutbasedir/dracut-functions.sh"
 }
 
@@ -2472,6 +2466,19 @@ if [[ $DRACUT_REPRODUCIBLE ]]; then
         CPIO_REPRODUCIBLE=1
     else
         dinfo "cpio does not support '--reproducible'. Resulting image will not be reproducible."
+    fi
+fi
+
+# Hardlink is mtime-sensitive; do it after the above clamp.
+if [[ $do_hardlink == yes ]] && command -v hardlink > /dev/null; then
+    dinfo "*** Hardlinking files ***"
+    hardlink "$initdir" 2>&1 | ddebug
+    dinfo "*** Hardlinking files done ***"
+
+    # Hardlink itself breaks mtimes on directories as we may have added/removed
+    # dir entries. Fix those up.
+    if [[ $DRACUT_REPRODUCIBLE ]]; then
+        clamp_mtimes "$initdir" -type d
     fi
 fi
 

dracut-ng/dracut

commit bed3e0a6d3dac1c1f203a113b59dc42d27ff275f

fix(dracut): correct order of "Creating image" log line
The log line "Creating image" is followed by "Hardlinking files":

```
[...]
dracut[I]: *** Including modules done ***
dracut[I]: *** Installing kernel module dependencies ***
dracut[I]: *** Installing kernel module dependencies done ***
dracut[I]: *** Resolving dependencies for executables and libraries ***
dracut[I]: *** Resolving dependencies for executables and libraries done ***
dracut[I]: *** Generating early-microcode cpio image ***
dracut[I]: *** Constructing GenuineIntel.bin ***
dracut[I]: *** Store current command line parameters ***
dracut[I]: *** Stripping files ***
dracut[I]: *** Stripping files done ***
dracut[I]: *** Creating image file '/var/tmp/dracut-test.doZhty/initramfs.testing' ***
dracut[I]: *** Hardlinking files ***
dracut[I]: *** Hardlinking files done ***
dracut[I]: Using auto-determined compression method 'zstd'
dracut[I]: *** Creating initramfs image file '/var/tmp/dracut-test.doZhty/initramfs.testing' done ***
```

Move the "Creating image" log line back to where it belonged:

```
[...]
dracut[I]: *** Including modules done ***
dracut[I]: *** Installing kernel module dependencies ***
dracut[I]: *** Installing kernel module dependencies done ***
dracut[I]: *** Resolving dependencies for executables and libraries ***
dracut[I]: *** Resolving dependencies for executables and libraries done ***
dracut[I]: *** Generating early-microcode cpio image ***
dracut[I]: *** Constructing GenuineIntel.bin ***
dracut[I]: *** Store current command line parameters ***
dracut[I]: *** Stripping files ***
dracut[I]: *** Stripping files done ***
dracut[I]: *** Hardlinking files ***
dracut[I]: *** Hardlinking files done ***
dracut[I]: *** Creating image file '/var/tmp/dracut-test.nfRABi/initramfs.testing' ***
dracut[I]: Using auto-determined compression method 'zstd'
dracut[I]: *** Creating initramfs image file '/var/tmp/dracut-test.nfRABi/initramfs.testing' done ***
```

Fixes: 9fdf683f6d2

dracut.sh
@@ -3133,8 +3133,6 @@ if [[ -e $outfile ]]; then
     outfile="${outfile}.tmp"
 fi
 
-dinfo "*** Creating image file '$outfile' ***"
-
 # Read list of files and echo them plus all leading directories.
 # The same directories might be printed multiple times (even with sorted input)!
 add_directories() {
@@ -3202,6 +3200,8 @@ if [[ $do_hardlink == yes ]] && command -v hardlink > /dev/null; then
         clamp_mtimes "$initdir" -type d
     fi
 fi
+
+dinfo "*** Creating image file '$outfile' ***"
 
 [[ $EUID != 0 ]] && cpio_owner="0:0"
 