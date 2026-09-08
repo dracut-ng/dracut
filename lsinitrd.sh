dracut-ng/dracut-ng

commit dbd9f0ee2c624e592a74b60080977b01307655c5

feat: warn in case 3cpio is present but not suitable
In case the 3cpio call fails, dracut will silently fall back to cpio.
This makes debugging harder.

So print a warning in case calling 3cpio fails. Print an info in case
3cpio does not support `--create`.

See also https://bugs.debian.org/1126301

lsinitrd.sh
@@ -97,8 +97,17 @@ while (($# > 0)); do
 done
 
 CPIO=cpio
-if 3cpio --help 2> /dev/null | grep -q -- --make-directories; then
-    CPIO=3cpio
+if command -v 3cpio > /dev/null; then
+    if help_output=$(3cpio --help); then
+        if echo "$help_output" | grep -q -- --make-directories; then
+            CPIO=3cpio
+        fi
+    elif command -v cpio > /dev/null; then
+        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories. Falling back to cpio."
+    else
+        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories."
+        CPIO=3cpio
+    fi
 fi
 
 if ! [[ $KERNEL_VERSION ]]; then

dracut.sh
@@ -2076,8 +2076,19 @@ elif [[ -n $persistent_policy && ! -d "/dev/disk/${persistent_policy}" ]]; then
 fi
 
 CPIO=cpio
-if 3cpio --help 2> /dev/null | grep -q -- --create; then
-    CPIO=3cpio
+if command -v 3cpio > /dev/null; then
+    if help_output=$(3cpio --help); then
+        if echo "$help_output" | grep -q -- --create; then
+            CPIO=3cpio
+        else
+            dinfo "3cpio does not support --create. Falling back to cpio."
+        fi
+    elif command -v cpio > /dev/null; then
+        dwarning "Calling '3cpio --help' failed. Cannot check if 3cpio supports --create. Falling back to cpio."
+    else
+        dwarning "Calling '3cpio --help' failed. Cannot check if 3cpio supports --create."
+        CPIO=3cpio
+    fi
 fi
 
 if [[ $enhanced_cpio == "yes" ]]; then

dracut-ng/dracut

commit 618455f098ede5f4bf4b4d29db633d2f7427155b

fix(lsinitrd): write warnings to stderr
Warnings should be written to stderr instead of stdout.

Fixes: dbd9f0ee2c62 ("feat: warn in case 3cpio is present but not suitable")

lsinitrd.sh
@@ -103,9 +103,9 @@ if command -v 3cpio > /dev/null; then
             EXTRACTOR=3cpio
         fi
     elif command -v cpio > /dev/null; then
-        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories. Falling back to cpio."
+        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories. Falling back to cpio." >&2
     else
-        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories."
+        echo "Warning: Calling '3cpio --help' failed. Cannot check if 3cpio supports --make-directories." >&2
         EXTRACTOR=3cpio
     fi
     unset threecpio_help_output