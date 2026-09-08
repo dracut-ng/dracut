dracut-ng/dracut

commit b208aad51c648e4d28c0415bd862298a7f302ebf

lsinitrd.sh: make use of the skipcpio utility
With the skipcpio utility, the whole contents of an initramfs with an
early cpio image can be displayed.

lsinitrd.sh
@@ -33,6 +33,9 @@ usage()
     } >&2
 }
 
+
+[[ $dracutbasedir ]] || dracutbasedir=/usr/lib/dracut
+
 sorted=0
 declare -A filenames
 
@@ -103,14 +106,72 @@ if ! [[ -f "$image" ]]; then
     exit 1
 fi
 
+extract_files()
+{
+    (( ${#filenames[@]} == 1 )) && nofileinfo=1
+    for f in ${!filenames[@]}; do
+        [[ $nofileinfo ]] || echo "initramfs:/$f"
+        [[ $nofileinfo ]] || echo "========================================================================"
+        $CAT $image | cpio --extract --verbose --quiet --to-stdout $f 2>/dev/null
+        ((ret+=$?))
+        [[ $nofileinfo ]] || echo "========================================================================"
+        [[ $nofileinfo ]] || echo
+    done
+}
+
+list_files()
+{
+    echo "========================================================================"
+    if [ "$sorted" -eq 1 ]; then
+        $CAT "$image" | cpio --extract --verbose --quiet --list | sort -n -k5
+    else
+        $CAT "$image" | cpio --extract --verbose --quiet --list | sort -k9
+    fi
+    ((ret+=$?))
+    echo "========================================================================"
+}
+
+
+if (( ${#filenames[@]} <= 0 )); then
+    echo "Image: $image: $(du -h $image | while read a b; do echo $a;done)"
+    echo "========================================================================"
+fi
+
 read -N 6 bin < "$image"
+case $bin in
+    $'\x71\xc7'*|070701)
+        CAT="cat --"
+        is_early=$(cpio --extract --verbose --quiet --to-stdout -- 'early_cpio' < "$image" 2>/dev/null)
+        if [[ "$is_early" ]]; then
+            if (( ${#filenames[@]} > 0 )); then
+                extract_files
+            else
+                echo "Early CPIO image"
+                list_files
+            fi
+            SKIP="$dracutbasedir/skipcpio"
+            if ! [[ -x $SKIP ]]; then
+                echo
+                echo "'$SKIP' not found, cannot display remaining contents!" >&2
+                echo
+                exit 0
+            fi
+        fi
+        ;;
+esac
+
+if [[ $SKIP ]]; then
+    read -N 6 bin < <($SKIP "$image")
+fi
+
 case $bin in
     $'\x1f\x8b'*)
         CAT="zcat --";;
     BZh*)
         CAT="bzcat --";;
     $'\x71\xc7'*|070701)
-        CAT="cat --";;
+        CAT="cat --"
+        ;;
     $'\x04\x22'*)
         CAT="lz4 -d -c";;
     *)
@@ -121,38 +182,32 @@ case $bin in
         ;;
 esac
 
+skipcpio()
+{
+    $SKIP "$@" | $ORIG_CAT
+}
+
+if [[ $SKIP ]]; then
+    ORIG_CAT="$CAT"
+    CAT=skipcpio
+fi
+
 ret=0
 
 if (( ${#filenames[@]} > 0 )); then
-    (( ${#filenames[@]} == 1 )) && nofileinfo=1
-    for f in ${!filenames[@]}; do
-        [[ $nofileinfo ]] || echo "initramfs:/$f"
-        [[ $nofileinfo ]] || echo "========================================================================"
-        $CAT $image | cpio --extract --verbose --quiet --to-stdout $f 2>/dev/null
-        ((ret+=$?))
-        [[ $nofileinfo ]] || echo "========================================================================"
-        [[ $nofileinfo ]] || echo
-    done
+    extract_files
 else
-    echo "Image: $image: $(du -h $image | while read a b; do echo $a;done)"
-    echo "========================================================================"
-    version=$($CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- '*lib/dracut/dracut-*' 2>/dev/null)
+    version=$($CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- 'lib/dracut/dracut-*' 'usr/lib/dracut/dracut-*' 2>/dev/null)
     ((ret+=$?))
     echo "Version: $version"
+    echo
     echo -n "Arguments: "
-    $CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- '*lib/dracut/build-parameter.txt' 2>/dev/null
+    $CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- 'lib/dracut/build-parameter.txt' 'usr/lib/dracut/build-parameter.txt' 2>/dev/null
     echo
     echo "dracut modules:"
-    $CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- '*lib/dracut/modules.txt' 2>/dev/null
+    $CAT "$image" | cpio --extract --verbose --quiet --to-stdout -- 'lib/dracut/modules.txt' 'usr/lib/dracut/modules.txt' 2>/dev/null
     ((ret+=$?))
-    echo "========================================================================"
-    if [ "$sorted" -eq 1 ]; then
-        $CAT "$image" | cpio --extract --verbose --quiet --list | sort -n -k5
-    else
-        $CAT "$image" | cpio --extract --verbose --quiet --list | sort -k9
-    fi
-    ((ret+=$?))
-    echo "========================================================================"
+    list_files
 fi
 
 exit $ret

commit 53da4b0c359e893e7f9fe11b5ff15180a47274ec

fix(lsinitrd): write complete error message to stderr
In case no extractor is found, an error message is printed on stderr,
but the leading and trailing newlines are printed on stdout. This seems
like unintentional to me.

This error message was moved around. It is now at the beginning of
`lsinitrd`. In this phase no text is printed yet. So the leading and
trailing newline are not needed and can be removed.

Fixes: b208aad51c64 ("lsinitrd.sh: make use of the skipcpio utility")

lsinitrd.sh
@@ -113,9 +113,7 @@ fi
 if ! [[ $EXTRACTOR ]]; then
     EXTRACTOR="$dracutbasedir/extractinitrd"
     if ! [[ -x $EXTRACTOR ]]; then
-        echo
         echo "Error: '$EXTRACTOR' not found, cannot continue!" >&2
-        echo
         exit 1
     fi
 fi