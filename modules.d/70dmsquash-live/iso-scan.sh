efd91e7e93412a5e32e7f2958b9a1fc0e4ff7bf6
Frederick Grose <fgrose@sugarlabs.org>
2026-09-10 11:03:23 -0400

    feat(iso-scan): avoid dirty/hibernated filesystem corruption
    Use filesystem driver mount options noload or norecovery on
      ext[43], f2fs, or xfs filesystems.
      Addresses issue dracut-ng#2562
    Check the md5sum of an isofile found on such a mounted filesystem.
    Unmount all filesystems after the target image loop has been setup.
    Save a link to the image loop device at /run/initramfs/isoloop.
    Also,
     - udevadm trigger the specific loop device for action=change,
     - warn if the isofile is not found.
     - quote full paths or parameters containing a variable expansion,
     - don't quote string constants for clarity & byte efficiency,
     - use || and && lists to short circuit conditionals
       avoiding unneeded if then logic,
     - send error messages to /dev/kmsg,
     - prefer [ "$var" ] || to [ -z "$var" ] && for byte efficiency & clarity.

--- modules.d/70dmsquash-live/iso-scan.sh
+++ modules.d/70dmsquash-live/iso-scan.sh
@@ -6,33 +6,53 @@ PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 isofile=$1
 
-[ -z "$isofile" ] && exit 1
+[ "$isofile" ] || exit 1
 
-ismounted "/run/initramfs/isoscan" && exit 0
-
-mkdir -p "/run/initramfs/isoscan"
+ismounted /run/initramfs/isoscan && exit 0
 
 do_iso_scan() {
-    local _name
-    local dev
+    local _name dev fstype
     for dev in /dev/disk/by-uuid/*; do
         _name=$(dev_unit_name "$dev")
-        [ -e /tmp/isoscan-"${_name}" ] && continue
-        : > /tmp/isoscan-"${_name}"
-        mount -t auto -o ro "$dev" "/run/initramfs/isoscan" || continue
-        if [ -f "/run/initramfs/isoscan/$isofile" ]; then
-            losetup -f "/run/initramfs/isoscan/$isofile"
-            udevadm trigger --action=add > /dev/null 2>&1
+        [ -e "/tmp/isoscan-${_name}" ] && continue
+        : > "/tmp/isoscan-${_name}"
+        fstype=$(blkid "$dev")
+        fstype="${fstype#* TYPE=\"}"
+        fstype="${fstype%%\"*}"
+        case $fstype in
+            ext[43]) opt=,noload ;;
+            xfs | f2fs) opt=,norecovery ;;
+        esac
+        dmesg -c > /dev/null
+        mount -m -t "$fstype" -o "ro$opt" "$dev" /run/initramfs/isoscan 2> /dev/kmsg || continue
+        if [ -f "${_isofile:="/run/initramfs/isoscan/$isofile"}" ]; then
+            [ "$opt" ] && {
+                command -v rd_iso_check > /dev/null || . /lib/img-lib.sh
+                dmesg | while read -r line; do
+                    case $line in
+                        *orphan* | *recovery* | *dirty* | *unclean* | *corrupt*)
+                            rd_iso_check "$_isofile"
+                            break
+                            ;;
+                    esac
+                done
+            }
+            loopdev=$(losetup -f)
+            losetup -r "$_isofile"
+            udevadm trigger "--name-match=$loopdev" --action=change --settle > /dev/kmsg 2>&1
             ln -s "$dev" /run/initramfs/isoscandev
+            ln -s "$loopdev" /run/initramfs/isoloop
+            umount -l /run/initramfs/isoscan
             rm -f -- "$job"
             exit 0
         else
-            umount "/run/initramfs/isoscan"
+            umount /run/initramfs/isoscan
         fi
     done
 }
 
 do_iso_scan
 
-rmdir "/run/initramfs/isoscan"
+rmdir /run/initramfs/isoscan
+warn "Unable to find $isofile."
 exit 1
